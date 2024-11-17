3615216131894b795e2026c499046ee1d81970d3
DexterFstone <Dexterf31415@gmail.com>
2024-11-16 22:14:40 -0800

    add connect to myket

--- source/godotmyket/app/build.gradle.kts
+++ source/godotmyket/app/build.gradle.kts
@@ -26,7 +26,13 @@ android {
         manifestPlaceholders["godotPluginPackageName"] = pluginPackageName
         buildConfigField("String", "GODOT_PLUGIN_NAME", "\"${pluginName}\"")
         setProperty("archivesBaseName", pluginName)
-
+//        val marketApplicationId = "ir.mservices.market"
+//        val marketBindAddress = "ir.mservices.market.InAppBillingService.BIND"
+//        manifestPlaceholders.apply {
+//            this["marketApplicationId"] = marketApplicationId
+//            this["marketBindAddress"] = marketBindAddress
+//            this["marketPermission"] = "${marketApplicationId}.BILLING"
+//        }
     }
 
     compileOptions {
@@ -41,6 +47,7 @@ android {
 dependencies {
     implementation("org.godotengine:godot:4.2.0.stable")
     // TODO: Additional dependencies should be added to export_plugin.gd as well.
+    implementation("com.github.myketstore:myket-billing-client:1.6")
 
     testImplementation("junit:junit:4.13.2")
 }
--- source/godotmyket/app/src/main/java/com/example/godotmyket/GodotMyket.java
+++ source/godotmyket/app/src/main/java/com/example/godotmyket/GodotMyket.java
@@ -1,4 +1,59 @@
 package com.example.godotmyket;
 
-public class GodotMyket {
+import android.util.Log;
+
+import org.godotengine.godot.Godot;
+import org.godotengine.godot.plugin.GodotPlugin;
+import org.godotengine.godot.plugin.SignalInfo;
+import org.godotengine.godot.plugin.UsedByGodot;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import ir.myket.billingclient.IabHelper;
+import ir.myket.billingclient.util.IabResult;
+
+public class GodotMyket extends GodotPlugin {
+    private IabHelper mHelper;
+    private String TAG = "godot";
+
+    /**
+     * Base constructor passing a {@link Godot} instance through which the plugin can access Godot's
+     * APIs and lifecycle events.
+     *
+     * @param godot
+     */
+    public GodotMyket(Godot godot) {
+        super(godot);
+    }
+
+    @Override
+    public String getPluginName() {
+        return "GodotMyket";
+    }
+
+    @Override
+    public Set<SignalInfo> getPluginSignals() {
+        Set<SignalInfo> signals = new HashSet<>();
+        signals.add(new SignalInfo("connection_succeed"));
+        signals.add(new SignalInfo("connection_failed", String.class));
+        return signals;
+    }
+
+    @UsedByGodot
+    public void connect_to_myket(String public_key) {
+        mHelper = new IabHelper(getActivity(), public_key);
+        mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+            @Override
+            public void onIabSetupFinished(IabResult result) {
+                if (mHelper == null) return;
+                if (result.isSuccess()) {
+                    emitSignal("connection_succeed");
+                }
+                else if (result.isFailure()) {
+                    emitSignal("connection_failed", result.getMessage());
+                }
+            }
+        });
+    }
 }
